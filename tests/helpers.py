4bc7d849acb70a84a3b42c2b7187454f477e15e2
koya <koyexes@gmail.com>
2022-03-14 11:27:13 +0100

    Adds helper functions for tests

---
+++ tests/helpers.py
@@ -0,0 +1,39 @@
+def get_patent_response_sample(record_total_quantity):
+    return {
+        "results": [
+            {
+                "inventionSubjectMatterCategory": "reissue",
+                "patentApplicationNumber": "US14046669",
+                "filingDate": "10-04-2013",
+                "mainCPCSymbolText": "H02K3/12",
+                "furtherCPCSymbolArrayText": [
+                    "H02K3/28",
+                    "H02K15/0081"
+                ],
+                "inventorNameArrayText": [
+                    "Tanaka Yuichiro",
+                    "Ishigami Takashi",
+                    "Nakayama Kenichi",
+                    "Saito Yasuyuki"
+                ],
+                "abstractText": [
+                    "Each pair of coils mutually connected in a stator winding is arranged in a fashion that a first coil of each pair of coils has an inner-circumferential-side coil terminal () led out from an inner-circumferential-side slot position in the direction of a coil end () of the stator winding, and that a second coil of each pair of coils has an outer-circumferential-side coil terminal () led out from an outer-circumferential-side slot position in the direction of the coil end () of the stator winding for connection to the inner-circumferential-side coil terminal (), wherein there is provided a coil terminal connection structure in which the inner-circumferential-side coil terminal () is connected to the outer-circumferential-side coil terminal () across the coil end (), and joint parts (a, a) thereof are bent close to the coil end."
+                ],
+                "assigneeEntityName": "Hitachi, Ltd.",
+                "assigneePostalAddressText": "Tokyo, JP",
+                "inventionTitle": "Rotating electric apparatus and method for connecting stator coils thereof",
+                "filelocationURI": "https://dh-opendata.s3.amazonaws.com/grant_pdf/grant_pdf_20170103/P20170103-20170103/RE/046/265/RE046265.pdf",
+                "archiveURI": "https://bulkdata.uspto.gov/data/patent/grant/redbook/bibliographic/2017/ipgb20170103_wk01.zip",
+                "claimText": [
+                    "1. A rotating electric apparatus comprising:a stator having a stator winding comprising a plurality of coils connected in a plurality of slots formed in a stator iron core; anda rotor rotatably disposed in the inside of said stator; wherein, each pair of coils mutually connected in said stator winding is arranged in a fashion thata first coil of each said pair of coils has a top-side coil terminal at a top-side position of said slot, "
+                ],
+                "descriptionText": [
+                    "1. Field of the InventionThe present invention relates to a rotating electric apparatus such as a motor or generator, and a method for connecting stator coils thereof.2. Description of the Related ArtThe types of stator windings are generally classified into a concentrated winding arrangement and a distributed winding arrangement; in the former, coils are concentrated per magnetic pole tooth, and in the latter, coils are provided across a plurality of slots so that different-phase or same-phase coils are made to overlap one another at a coil end position."
+                ],
+                "grantDocumentIdentifier": "USRE046265E1",
+                "grantDate": "01-03-2017",
+                "patentNumber": "RE046265"
+            }
+        ],
+        "recordTotalQuantity": record_total_quantity
+    }
